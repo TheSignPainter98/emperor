183de9f3ff28c8d90475a21b1b6264c5c150c099
TheSignPainter <edjones98@gmail.com>
2019-03-19 01:03:26 +0000

    Re-done make files

--- src/emperor/emperor.pyx
+++ src/emperor/emperor.pyx
@@ -1,5 +1,6 @@
 
 # cimport actions.actions
+import argparse
 import parser 
 
 cdef extern from "./actions/actions.c":
@@ -7,5 +8,9 @@ cdef extern from "./actions/actions.c":
 
 def main(args:list) -> int:
 	print('Hello, world!')
+	action()
+	print('Make some input pls!> ')
+	string = input()
+	print(f'You inputted: {string}')
 	return 0
 
