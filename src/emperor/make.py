32340e22def8e1451f273b61629b212410a6ccc3
TheSignPainter <edjones98@gmail.com>
2019-03-21 01:27:54 +0000

    Adding modules

--- src/emperor/make.py
+++ src/emperor/make.py
@@ -2,7 +2,10 @@
 
 from distutils.core import setup
 from Cython.Build import cythonize
+from distutils.extension import Extension
 
+sourcefiles = ['./parser/emperor.tab.c', './parser/emperor.yy.c', './emperor.pyx']
+extensions = [Extension('emperor', sourcefiles)]
 setup(
-	ext_modules = cythonize(['./emperor.pyx'])
+	ext_modules = cythonize(extensions)
 )
