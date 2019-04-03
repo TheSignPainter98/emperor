09e58a2d728bfada8a4b34d632169235b4925cc5
TheSignPainter <edjones98@gmail.com>
2019-04-03 23:17:43 +0100

    Man detail, more optoins

--- src/emperor/emperor.pyx
+++ src/emperor/emperor.pyx
@@ -2,6 +2,7 @@
 import sys
 
 emperorVersion:str = '0.1.0'
+emperorVersionString:str = 'emperor 0.1.0\nWritten by Edward Jones\n\nCopyright (C) 2015 Edward Jones\nThis is free software; see the source for copying conditions.  There is NO\nwarranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.'
 
 ################################################################################
 import argparse
@@ -13,37 +14,32 @@ import textwrap
 STDIN_FLAG = '-'
 
 class TroffArgumentParser(argparse.ArgumentParser):
-	def __init__(self, *args, **kwargs):
+	def __init__(self, licence:str=None, seeAlso:str=None, date:str=date.today().strftime('%d %B %Y'), bugs:str=None, *args, **kwargs):
 		super(TroffArgumentParser, self).__init__(*args, **kwargs)
-		if 'date' in kwargs:
-			self.date = kwargs['date']
-		else:
-			self.date = date.today().strftime('%d %B %Y')
+		self.licence = licence
+		self.date = date
+		self.seeAlso = seeAlso
+		self.bugs = bugs
 
 	def toTroff(self) -> str:
-		# print(inspect.getmembers(self))
-		# print('===')
-		# print(self.__dict__)
-		# print('===')
 		options:[argparse.Action] = self._actions
 		options.sort(key=lambda option : self._checkMandatory(option))
-		# print(self.__dict__['prog'])
-		# print(self.__dict__['description'])
-		# print(self.__dict__['prefix_chars'])
-		# print('===')
-		# for optionKey in options:
-		# 	option:argparse._HelpAction = options[optionKey]
-		# 	print(f'\t{option.option_strings}\t{option.nargs}\t{option.choices}\t{option.dest}\t{option.metavar}\n\t\t{option.help}')
-		
-		troffOutput:str = f'.TH {self.prog} 1 "{self.date}"\n'
+
+		troffOutput:str = self._header(self.prog, self.date, self.licence)
 		troffOutput += self._name(self.prog, self.description)
 		troffOutput += self._synopsis(self.prog, options)
 		troffOutput += self._description(self.description)
 		troffOutput += self._options(options)
+		troffOutput += self._seeAlso(self.seeAlso)
+		troffOutput += self._bugs(self.bugs)
+		troffOutput += self._authors(self.epilog)
 		return troffOutput
 
 	def _checkMandatory(self, action:argparse.Action) -> bool:
 		return True if action.option_strings == [] else False
+
+	def _header(self, program:str, date:str, licence:str) -> str:
+		return f'{licence}\n.TH {self.prog.upper()} 1 "{self.date}"\n'
 
 	def _name(self, prog:str, description:str) -> str:
 		return f'.SH "NAME"\n\\fB{prog}\\fP - {description}\n'
@@ -78,42 +74,86 @@ class TroffArgumentParser(argparse.ArgumentParser):
 			optionString:str = self._formatList(option.option_strings) if option.option_strings != [] else option.metavar
 			optionString = r'\fB' + optionString + r'\fP '
 			if option.choices is not None:
-				choices:str = self._formatList(option.choices)
+				choices:str = self._formatList(option.choices, separator=',')
 				optionString += r'\fI{' + choices + r'}\fP'
+			elif option.nargs is None:
+				optionString += r'\fI' + (option.metavar.upper() if option.metavar is not None else option.dest.upper()) + r'\fP'
+				pass
+			elif option.nargs == '*':
+				pass
+			elif option.nargs == '+':
+				pass
+			elif int(option.nargs) >= 0:
+				pass
+
 			optionDescription:str = f'\n.TP\n{optionString}\n{option.help}'
 			description += optionDescription
 			firstOption = False
 			previousWasMandatory = self._checkMandatory(option)
-		return description
+		return description + '\n'
 
-	def _formatList(self, lst:[str]) -> str:
+	def _formatList(self, lst:[str], separator:str=', ') -> str:
 		formattedList:str = ''
 		if lst is not None:
 			formattedList = ''
 			firstItem:bool = True
 			for item in lst:
 				if not firstItem:
-					formattedList += ', '
+					formattedList += separator
 				firstItem = False
 				formattedList += item
 		return formattedList
 
+	def _seeAlso(self, seeAlso:str) -> str:
+		return f'.SH "SEE ALSO"\n{seeAlso}\n' if seeAlso is not None else ''
+
+	def _bugs(self, bugs:str) -> str:
+		return f'.SH "BUGS"\n{bugs}\n' if bugs is not None else ''
+
+	def _authors(self, epilog:str) -> str:
+		return f'.SH "AUTHOR"\n{epilog}\n' if epilog is not None else ''
+
 
 def parseArguments(args:[str]) -> argparse.Namespace:
 	parser:TroffArgumentParser = TroffArgumentParser(
-		# prog = 'emperor',
-		description = f'''This is the compiler for the Emperor language v{emperorVersion}''',
-		epilog = '''This is maintained by Edward Jones, and source code can be found at https://github.com/TheSignPainter98/emperor'''
+		licence=(
+			'.\\" Copyright (c) <year>, <copyright holder>\n'
+			'.\\"\n'
+			'.\\" %%%LICENSE_START(GPLv2+_DOC_FULL)\n'
+			'.\\" This is free documentation; you can redistribute it and/or\n'
+			'.\\" modify it under the terms of the GNU General Public License as\n'
+			'.\\" published by the Free Software Foundation; either version 2 of\n'
+			'.\\" the License, or (at your option) any later version.\n'
+			'.\\"\n'
+			'.\\" The GNU General Public License\'s references to "object code"\n'
+			'.\\" and "executables" are to be interpreted as the output of any\n'
+			'.\\" document formatting or typesetting system, including\n'
+			'.\\" intermediate and printed output.\n'
+			'.\\"\n'
+			'.\\" This manual is distributed in the hope that it will be useful,\n'
+			'.\\" but WITHOUT ANY WARRANTY; without even the implied warranty of\n'
+			'.\\" MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n'
+			'.\\" GNU General Public License for more details.\n'
+			'.\\"\n'
+			'.\\" You should have received a copy of the GNU General Public\n'
+			'.\\" License along with this manual; if not, see\n'
+			'.\\" <http://www.gnu.org/licenses/>.\n'
+			'.\\" %%%LICENSE_END'
+		),
+		description = f'''Compiler for the Emperor language v{emperorVersion}''',
+		seeAlso = r'''\fBgcc\fR(1), \fBdux\fP(1), \fBbison\fP(1), \fBflex\fP(1)''',
+		bugs = f'''There are no known bugs at this time! :D If you find any, however, please report them at <https://github.com/TheSignPainter98/emperor/issues>''',
+		epilog = '''This is maintained by Edward Jones, and source code can be found at <https://github.com/TheSignPainter98/emperor>'''
 	)	
 	parser.add_argument('-v', '--verbose', action='store_true', help='Output verbosity')
-	parser.add_argument('files', metavar='file', type=str, nargs='*', help='A file to compile')
+	parser.add_argument('-V', '--version', action='store_true', help='Output version and exit')
+	parser.add_argument('-m', '--man_page', dest='output_man', action='store_true', help='Output man page formatted in Troff and exit')
 	parser.add_argument('-O', '--optimisation', choices=[ 's', '0', '1', '2' ], default=0, dest='optimisation', help='Compiler optimisation level (see gcc)')
 	parser.add_argument('-c', '--to-c', action='store_true', dest='compileCOnly', help='Translate to C (skips compilation step)')
-	parser.add_argument('-o', '--output', type=str, dest='outputFile', default=None, help='Specify output file')
-
-	print(parser.toTroff())
+	parser.add_argument('-o', '--output', type=str, dest='outputFile', metavar='OUTPUT_FILE', default=None, action='store', help='Specify output file')
+	parser.add_argument('files', metavar='file', type=str, nargs='*', help='A file to compile')
 	
-	return _validateArgs(parser.parse_args(args))
+	return _validateArgs(parser, parser.parse_args(args))
 
 def _unique(inputList:[str], key=lambda x : x) -> [str]:
 	uniqueList:[str] = []
@@ -133,7 +173,15 @@ def _sanitiseFilePath(filePath:str) -> str:
 	else:
 		return filePath
 
-def _validateArgs(arguments:argparse.Namespace) -> argparse.Namespace:
+def _validateArgs(parser:TroffArgumentParser, arguments:argparse.Namespace) -> argparse.Namespace:
+	if arguments.version:
+		print(emperorVersionString)
+		sys.exit(0)
+
+	if arguments.output_man:
+		print(parser.toTroff(), end='')
+		sys.exit(0)
+
 	if arguments.files == []:
 		arguments.files = [STDIN_FLAG]
 	else:
@@ -163,14 +211,14 @@ def _validateArgs(arguments:argparse.Namespace) -> argparse.Namespace:
 	
 def main(args:[str]) -> int:
 	arguments:argparse.NameSpace = parseArguments(args)
-	# print('Argument is verbose <==> %s' %('true' if arguments.verbose else 'false'))
+	print('Argument is verbose <==> %s' %('true' if arguments.verbose else 'false'))
 
-	# print('Hello, world! This is Cython!')
+	print('Hello, world! This is Cython!')
 
-	# print(f'This will be running on input files: {arguments.files}')
-	# print(f'Running at optimisation level: {arguments.optimisation}')
-	# print(f'Compiling to C only?: {arguments.compileCOnly}')
-	# print(f'Output will be written in \"{arguments.outputFile}\"')
+	print(f'This will be running on input files: {arguments.files}')
+	print(f'Running at optimisation level: {arguments.optimisation}')
+	print(f'Compiling to C only?: {arguments.compileCOnly}')
+	print(f'Output will be written in \"{arguments.outputFile}\"')
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
